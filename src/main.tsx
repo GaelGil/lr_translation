1453cd2557d04f6b08823be9db3f73f42d40d461
Gael Gil <gaelgilbello27@gmail.com>
2026-01-19 11:51:20 -0800

    adding stuff

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
 import "./main.css";
-
+import "@mantine/core/styles.css";
+import { theme } from "./theme";
+import { MantineProvider } from "@mantine/core";
+import {
+  //   MutationCache,
+  //   QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 const router = createRouter({ routeTree });
 declare module "@tanstack/react-router" {
   interface Register {
@@ -11,8 +19,23 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const queryClient = new QueryClient({
+  //   queryCache: new QueryCache({
+  //     onError: handleApiError,
+  //   }),
+  //   mutationCache: new MutationCache({
+  //     onError: handleApiError,
+  //   }),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        {/* <ColorModeProvider initialColorScheme="light"> */}
+        <RouterProvider router={router} />
+        {/* </ColorModeProvider> */}
+      </QueryClientProvider>
+    </MantineProvider>
   </StrictMode>,
 );
