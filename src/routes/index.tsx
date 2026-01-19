f4729009d3a5ce9135bc0cf36cfb4a2781ee30ac
Gael Gil <gaelgilbello27@gmail.com>
2026-01-19 11:36:31 -0800

    updating stuff

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,24 @@
 // routes/index.tsx
 import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@mantine/core";
+import { AppShell, Text } from "@mantine/core";
 
 export const Route = createFileRoute("/")({
   component: HomePage,
 });
 
 function HomePage() {
-  return <AppShell></AppShell>;
+  return (
+    <AppShell>
+      <AppShell.Header>
+        <Text c="black">Header</Text>
+      </AppShell.Header>
+      <AppShell.Navbar>
+        <Text>Navbar</Text>
+      </AppShell.Navbar>
+      <Text>Sidebar</Text>
+      <AppShell.Main>
+        <Text>Main</Text>
+      </AppShell.Main>
+    </AppShell>
+  );
 }
