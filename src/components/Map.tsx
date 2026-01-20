e45f42911ba72ddec3988916ee63eafde4f97fa4
Gael Gil <gaelgilbello27@gmail.com>
2026-01-20 14:27:10 -0800

    working on map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,13 @@
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMapEvents,
+  CircleMarker,
+} from "react-leaflet";
 import L from "leaflet";
 import { useState } from "react";
+import { Menu } from "@mantine/core";
 
 type Point = { id: string; lat: number; lng: number };
 
@@ -28,16 +35,32 @@ function ClickHandler({ addPoint }: { addPoint: (p: Point) => void }) {
   return null;
 }
 
-export default function LeafletMap() {
-  const [points, setPoints] = useState<Point[]>([]);
-  // const bounds: [[number, number], [number, number]] = [
-  //   [30.7749, -122.4194], // SW corner
-  //   [20.7749, -112.4194], // NE corner
-  // ];
+type MarkerPoint = {
+  id: string;
+  lat: number;
+  lng: number;
+};
 
+export default function LeafletMap() {
+  const [markers, setMarkers] = useState<MarkerPoint[]>([
+    { id: "1", lat: 37.7702, lng: -122.4636 },
+    { id: "2", lat: 37.7602, lng: -122.4273 },
+  ]);
+  const [hoveredId, setHoveredId] = useState<string | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = (index: number) => {
+    if (isOpen) {
+      setHoveredId(null);
+    } else {
+      setIsOpen(!isOpen);
+    }
+
+    setHoveredId(String(index));
+  };
   return (
     <MapContainer
-      center={[37.7749, -122.4194]}
+      center={[37.76062130454376, -122.42187034608556]}
       zoom={13}
       // bounds={bounds}
       // maxBounds={bounds}
@@ -46,10 +69,42 @@ export default function LeafletMap() {
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      <ClickHandler addPoint={(p) => setPoints((arr) => [...arr, p])} />
+      {/* <ClickHandler addPoint={(p) => setPoints((arr) => [...arr, p])} /> */}
 
-      {points.map((p) => (
-        <Marker key={p.id} position={[p.lat, p.lng]} />
+      {markers.map((p) => (
+        <>
+          <CircleMarker
+            key={p.id}
+            center={[p.lat, p.lng]}
+            radius={6}
+            pathOptions={{
+              color: "#000",
+              fillColor: "#ff0000",
+              fillOpacity: 1,
+            }}
+          />
+          {isOpen && hoveredId === p.id && (
+            <>
+              <Menu
+                width={200}
+                position="top-end"
+                shadow="md"
+                onClose={() => setIsOpen(false)}
+              >
+                <Menu.Target>
+                  <button onClick={() => handleClick(Number(p.id))}>
+                    Open menu
+                  </button>
+                </Menu.Target>
+
+                <Menu.Dropdown>
+                  <Menu.Label>Actions</Menu.Label>
+                  <Menu.Item onClick={() => setIsOpen(false)}>Close</Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </>
+          )}
+        </>
       ))}
     </MapContainer>
   );
