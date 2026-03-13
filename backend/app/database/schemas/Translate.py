65a0f398eb68f999c103292ab2329325f64b19e4
Gael Gil <gaelgilbello27@gmail.com>
2026-03-13 16:39:35 -0700

    updated stuff

--- backend/app/database/schemas/Translate.py
+++ backend/app/database/schemas/Translate.py
@@ -1,6 +1,6 @@
-from sqlmodel import Field, SQLModel
 from enum import Enum
-
+from sqlmodel import Field, SQLModel
+from sqlalchemy import Column, Text
 
 
 
@@ -8,13 +8,17 @@ from enum import Enum
 class TranslationStatus(Enum):
     INPROGRESS= "inporgress"
     FAILED = "failed"
-    COMPLTED = "completed"
+    COMPLETE = "completed"
 
 
 
 class TranslateRequest(SQLModel):
     text: str = Field(nullable=False)
 
+
+class MessageBase(SQLModel):
+    text: str = Field(sa_column=Column(Text, nullable=False))
+    status: TranslationStatus = Field(default=TranslationStatus.COMPLETE, nullable=False)
 
 class TranslateResponse(SQLModel):
     message: str
