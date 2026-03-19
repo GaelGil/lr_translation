668bd39715042f6d9ec6c25bf0c2dba90bd5a8d5
Gael Gil <gaelgilbello27@gmail.com>
2026-03-19 12:48:04 -0700

    updates

--- backend/app/api/translation/TranslationService.py
+++ backend/app/api/translation/TranslationService.py
@@ -27,24 +27,13 @@ class TranslationService:
             is_complete=False,
         )
         words = text.split()
-        print(f"WORDS: {words}")
         final_word = words[-1]
         for word in words:
-            print(f"WORDSSSSSSSSSSSS: {word}")
             await self.manager.stream_response_chunk(
                 translate_id=translate_id,
                 chunk=word,
                 is_complete=word == final_word,
             )
-            await asyncio.sleep(2)
-
-        # for i, word in enumerate(words):
-        #     print(f"DEBUG: WORD: {word}")
-        #     await self.manager.stream_response_chunk(
-        #         message_id=message_id,
-        #         chunk=word + " " if i < len(words) - 1 else word,
-        #         is_complete=i == len(words) - 1,
-        #     )
-        #     if i < len(words) - 1:
+            await asyncio.sleep(5)
 
         return text
